f7b09e71fef1617e0f6b13fddb7ac07b793d5bca
Ranu <Ranu@Ranu-PC>
2018-05-25 21:55:38 +0530

    star

---
+++ newproject/src/program/star.java
@@ -0,0 +1,20 @@
+package program;
+
+import org.testng.annotations.Test;
+
+public class star {
+	
+	@Test
+	public void star(){
+	for (int i=1; i<=4;i++){
+		
+		for(int j=1; j<=i;j++){
+			
+			System.out.print("*");
+			
+		}
+		System.out.println("");
+	}
+
+   }
+	}
